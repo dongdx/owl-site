cf24dc4820325cf324b5954b82a84ef99712cd8c
yangyi <yangyi.cn.gz@gmail.com>
2016-02-03 15:41:23 +0800

    replace Parameter\Checker with Parameter\Validator

--- Controller.php
+++ Controller.php
@@ -2,7 +2,7 @@
 abstract class Controller {
     protected function checkParameters(array $parameters, array $options) {
         try {
-            (new \Owl\Parameter\Checker)->execute($parameters, $options);
+            (new \Owl\Parameter\Validator)->execute($parameters, $options);
         } catch (\Owl\Parameter\Exception $exception) {
             throw \Owl\Http\Exception::factory(400, $exception);
         }
